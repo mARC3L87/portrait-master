fecc3e4f58787471d6583047a97cdd8c6ca5f34c
Marcel Łukomski <marcel.lukomski@gmail.com>
2020-11-22 21:36:42 +0100

    Fix security to web page

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -11,9 +12,26 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
+      if(title.length > 25 && author.length > 50) {
+        throw new Error('Wrong value');
+      }
+      const reTitleAuthor =/^[a-zA-Z ]*$/;
+      if(!reTitleAuthor.test(title && author)) {
+        throw new Error('Wrong value');
+      }
+      const reEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+      if(!reEmail.test(email)) {
+        throw new Error('Wrong value');
+      }
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if(fileExt == 'jpg' || fileExt == 'gif' || fileExt == 'png') {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        throw new Error('Wrong file format');
+      }
+     
 
     } else {
       throw new Error('Wrong input!');
@@ -43,11 +61,26 @@ exports.vote = async (req, res) => {
 
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    const voter = await Voter.findOne({ user: req.clientIp });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+
     else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
+      if (voter) {
+        if (voter.votes.includes(photoToUpdate._id)) {
+          res.status(500).json(err);
+        } else {
+          voter.votes.push(photoToUpdate._id);
+          photoToUpdate.votes++;
+          photoToUpdate.save();
+          res.send({ message: 'OK' });
+        }
+      } else {
+        const newVoter = new Voter({ user: req.clientIp, votes: [photoToUpdate._id]});
+        await newVoter.save();
+        photoToUpdate.votes++;
+        photoToUpdate.save();
+        res.send({ message: 'OK' });
+      }
     }
   } catch(err) {
     res.status(500).json(err);
